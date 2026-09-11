c2e49e5138b304423bcfa898e7b3ddd64c011eb0
ELELEARG <elejecontacto@gmail.com>
2026-09-11 17:41:18 -0300

    Fixes: cierre automatico 25/09, timeout de envio, placeholder barrio

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // URL de la App web de Google Apps Script que guarda las postulaciones en Google Sheets.
 window.ELEJE_CONFIG = {
+  // Cierre de la convocatoria (hora argentina). Pasada esta fecha el formulario deja de aceptar postulaciones.
+  cierre: "2026-09-25T23:59:59-03:00",
   endpoint: "https://script.google.com/macros/s/AKfycbwzngyzIA53JP8pMy81E4QY5YsMoAWTLa2tExGHipG1kJGZwKwmYCXSyF5Y-BE0ZD-R/exec"
 };
